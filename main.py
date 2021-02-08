6c62cc31b0329e954e430c06188cb97b66148cca
MD ARMAN <muhammadarman187@gmail.com>
2021-02-08 22:11:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import pikepdf
+from tqdm import tqdm
+
+passwords = [line.strip() for line in open("wordlist.txt")]
+pdf-file = input("Enter the PDF filename with .pdf extension:\n")
+
+for password in tqdm(passwords, "Decrypting PDF"):
+  try:
+    with pikepdf.open(pdf-file, passwords=passwords) as pdf:
+      print("\n[+] Password Found:", passwords)
+      break
+  except pikepdf._qpdf.PasswordError as e:
+    continue
